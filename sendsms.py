bcb222478febc1efae0303927a7836007aaa4c6f
Hristo Borisov <ico.borisov@gmail.com>
2015-06-14 12:26:03 +0000

    adding sms daemon

--- sendsms.py
+++ sendsms.py
@@ -4,37 +4,40 @@ import time
 import subprocess
 import os
 from curses import ascii
+import sys
 
-DEVNULL = open(os.devnull, 'wb')
+def sendsms(phoneNumber, text):
+	ser = serial.Serial('/dev/ttyUSB1', 460800, timeout=1)
+	print ser.name
 
-ser = serial.Serial('/dev/ttyUSB1', 460800, timeout=1)
-print ser.name
+	ser.write("AT\r\n")
+	line = ser.readline()
+	line = ser.readline()
+	print line
 
-ser.write("AT\r\n")
-line = ser.readline()
-print line
-line = ser.readline()
-print line
+	ser.write("AT+CMGF=1\r\n")
+	line = ser.readline()
+	line = ser.readline()
+	print line
+	print ser.readline()
 
-ser.write("AT+CMGF=1\r\n")
-line = ser.readline()
-print line
-line = ser.readline()
-print line
-print ser.readline()
+	ser.write('AT+CMGS="%s"\r\n' %phoneNumber)
+	ser.write(text)
+	ser.write(ascii.ctrl('z'))
+	time.sleep(3)
+	print ser.readline()
+	print ser.readline()
+	print ser.readlines()
 
-#ser.write("AT+ZMSGL=1,2")
-#print ser.readline()
-#print ser.readline()
-#print ser.readline()
+	ser.close()
+	return
 
-ser.write('AT+CMGS="%s"\r\n' %"+359882506400")
-ser.write("aaaa")
-ser.write(ascii.ctrl('z'))
-time.sleep(3)
-print ser.readline()
-print ser.readline()
-print ser.readlines()
+if __name__ == "__main__":
+	if len(sys.argv) != 3:
+		print "Wrong number of arguments"
+		print "Usage: sendsms.py <phone number> <message text>"
+		sys.exit(-1)
 
-ser.close()
-DEVNULL.close()
+	phoneNumber = sys.argv[1]
+	smsText = sys.argv[2]
+	sendsms(phoneNumber, smsText)
